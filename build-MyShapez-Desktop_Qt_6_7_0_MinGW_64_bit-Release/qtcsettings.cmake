e5fb7b66fea781bfc4d52fe5cc51bf9a70eb99b8
Qianyu Jiang <114326441+jiangqianyu@users.noreply.github.com>
2024-01-16 14:58:02 +0800

    Add files via upload
    build-MyShapez-Desktop_Qt_6_7_0_MinGW_64_bit-Release

---
+++ build-MyShapez-Desktop_Qt_6_7_0_MinGW_64_bit-Release/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/Daisydaisydaisy/cpp2023fall/MyShapez/build-MyShapez-Desktop_Qt_6_7_0_MinGW_64_bit-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "D:/Daisydaisydaisy/Qt/6.7.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "D:/Daisydaisydaisy/Qt/6.7.0/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "D:/Daisydaisydaisy/Qt/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "D:/Daisydaisydaisy/Qt/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
